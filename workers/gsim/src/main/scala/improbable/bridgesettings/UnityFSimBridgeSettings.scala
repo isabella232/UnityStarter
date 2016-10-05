0e73769b1404075fa8a8dade978708cf3a9a6051
Chong-U Lim <chong-u@improbable.io>
2016-10-05 15:10:44 +0100

    Add project files from BlankProject

---
+++ workers/gsim/src/main/scala/improbable/bridgesettings/UnityFSimBridgeSettings.scala
@@ -0,0 +1,29 @@
+package improbable.bridgesettings
+
+import improbable.fapi.bridge._
+import improbable.fapi.network.RakNetLinkSettings
+import improbable.unity.fabric.AuthoritativeEntityOnly
+import improbable.unity.fabric.bridge.FSimAssetContextDiscriminator
+import improbable.unity.fabric.engine.EnginePlatform
+import improbable.unity.fabric.satisfiers.SatisfyPhysics
+
+object UnityFSimBridgeSettings extends BridgeSettingsResolver {
+
+  private val fSimEngineBridgeSettings = BridgeSettings(
+    FSimAssetContextDiscriminator(),
+    RakNetLinkSettings(),
+    EnginePlatform.UNITY_FSIM_ENGINE,
+    SatisfyPhysics,
+    AuthoritativeEntityOnly(),
+    MetricsEngineLoadPolicy,
+    PerEntityOrderedStateUpdateQos
+  )
+
+  override def engineTypeToBridgeSettings(engineType: String, metadata: String): Option[BridgeSettings] = {
+    if (engineType == EnginePlatform.UNITY_FSIM_ENGINE) {
+      Some(fSimEngineBridgeSettings)
+    } else {
+      None
+    }
+  }
+}
